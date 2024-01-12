73f67a6831dbcffec25414097ea2f159514588a5
Patrick Jiang <patrick.jiang.2020@gmail.com>
2024-01-12 12:40:16 -0600

    implemented basic course listing

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,9 @@
+import Course from './Course';
+
+const CourseList = ({courses}) => (
+    <div>
+        { Object.entries(courses).map(([id, course]) => <Course key={id} course={course}/>) }
+    </div>
+);
+
+export default CourseList
