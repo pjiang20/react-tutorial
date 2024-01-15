bac50c58c2aa61eec0fd7843b72c98c50b8f0965
Patrick Jiang <patrick.jiang.2020@gmail.com>
2024-01-15 17:51:03 -0600

    implemented data fetching

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -6,7 +6,7 @@ const Course = ({course}) => (
             <h4 className="card-title">{course.term} CS {course.number}</h4>
             <p className="card-text">{course.title}</p>
         </div>
-        <div class="card-footer bg-transparent">{course.meets}</div>
+        <div className="card-footer bg-transparent">{course.meets}</div>
     </div>
 );
 
